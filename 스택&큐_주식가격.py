108994326ae2d9b7dd89616769dc5fa6330b550d
Junseon Kim <zzunvely@naver.com>
2019-08-25 23:36:08 +0900

    Add files via upload

---
+++ 스택&큐_주식가격.py
@@ -0,0 +1,11 @@
+#프로그래머스 > 코딩테스트 > 스택&큐> 주식가격
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+            if prices[i] >prices[j]:
+                break
+            else:
+                answer[i] +=1
+    return answer
